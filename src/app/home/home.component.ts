2310a67f18c57866774add1a81966e596b091a43
KromaStone <groot707cod@gmail.com>
2024-06-30 21:20:57 +0530

    Initialize the Portfolio work

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { HeroComponent } from '../hero/hero.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,HeroComponent,AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
