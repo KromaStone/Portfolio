87a4822f71afad77718f685a9d83ac1b5f557248
KromaStone <groot707cod@gmail.com>
2024-07-16 19:19:19 +0530

    Responsive Work

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,70 +7,84 @@ import { InputTextModule } from 'primeng/inputtext';
 import { CommonModule } from '@angular/common';
 import { RippleModule } from 'primeng/ripple';
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports:  [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent implements OnInit {
-  items: MenuItem[] | undefined;
+    items: MenuItem[] | undefined;
 
-  ngOnInit() {
-      this.items = [
-          {
-              label: 'Home',
-              icon: 'pi pi-home'
-          },
-          {
-              label: 'Features',
-              icon: 'pi pi-star'
-          },
-          {
-              label: 'Projects',
-              icon: 'pi pi-search',
-              items: [
-                  {
-                      label: 'Core',
-                      icon: 'pi pi-bolt',
-                      shortcut: '⌘+S'
-                  },
-                  {
-                      label: 'Blocks',
-                      icon: 'pi pi-server',
-                      shortcut: '⌘+B'
-                  },
-                  {
-                      label: 'UI Kit',
-                      icon: 'pi pi-pencil',
-                      shortcut: '⌘+U'
-                  },
-                  {
-                      separator: true
-                  },
-                  {
-                      label: 'Templates',
-                      icon: 'pi pi-palette',
-                      items: [
-                          {
-                              label: 'Apollo',
-                              icon: 'pi pi-palette',
-                              badge: '2'
-                          },
-                          {
-                              label: 'Ultima',
-                              icon: 'pi pi-palette',
-                              badge: '3'
-                          }
-                      ]
-                  }
-              ]
-          },
-          {
-              label: 'Contact',
-              icon: 'pi pi-envelope',
-              badge: '3'
-          }
-      ];
-  }
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+                icon: 'pi pi-home'
+            },
+            {
+                label: 'About',
+                icon: 'pi pi-star'
+            }, {
+                label: 'Skill',
+                icon: 'pi pi-star'
+            }, {
+                label: 'Services',
+                icon: 'pi pi-star'
+            },
+            {
+                label: 'Project',
+                icon: 'pi pi-star'
+            },
+            {
+                label: 'Contact',
+                icon: 'pi pi-star'
+            },
+            // {
+            //     label: 'Projects',
+            //     icon: 'pi pi-search',
+            //     items: [
+            //         {
+            //             label: 'Core',
+            //             icon: 'pi pi-bolt',
+            //             shortcut: '⌘+S'
+            //         },
+            //         {
+            //             label: 'Blocks',
+            //             icon: 'pi pi-server',
+            //             shortcut: '⌘+B'
+            //         },
+            //         {
+            //             label: 'UI Kit',
+            //             icon: 'pi pi-pencil',
+            //             shortcut: '⌘+U'
+            //         },
+            //         {
+            //             separator: true
+            //         },
+            //         {
+            //             label: 'Templates',
+            //             icon: 'pi pi-palette',
+            //             items: [
+            //                 {
+            //                     label: 'Apollo',
+            //                     icon: 'pi pi-palette',
+            //                     badge: '2'
+            //                 },
+            //                 {
+            //                     label: 'Ultima',
+            //                     icon: 'pi pi-palette',
+            //                     badge: '3'
+            //                 }
+            //             ]
+            //         }
+            //     ]
+            // },
+            // {
+            //     label: 'Contact',
+            //     icon: 'pi pi-envelope',
+            //     badge: '3'
+            // }
+        ];
+    }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  projectList = [
+    {
+      backgroundImage: 'images/responsiveWebDesign.jpg',
+      heading: 'Responsive Web Design',
+      description: 'Responsive Web Design ensures websites adapt to various devices and screen sizes using flexible layouts, grids, and CSS media queries.',
+      icon: 'fa-solid fa-share-from-square',
+    },
+    {
+      backgroundImage: 'images/performanceOpti.jpeg',
+      heading: 'Performance Optimization',
+      description: 'Performance Optimization improves site speed by reducing load times, optimizing assets, and employing techniques like caching, compression, and code minification.',
+      icon: 'fa-solid fa-share-from-square',
+    },
+    {
+      backgroundImage: 'images/databaseManagement.png',
+      heading: 'Database Management',
+      description: 'Database Management involves organizing, storing, and retrieving data efficiently while ensuring data integrity, security, and scalability for applications.',
+      icon: 'fa-solid fa-share-from-square',
+    },
+    {
+      backgroundImage: 'images/serverSideLogic.jpeg',
+      heading: 'Server Side Logic',
+      description: 'Server Side Logic manages data, executes business rules, and handles user requests on the server for efficient web application operations.',
+      icon: 'fa-solid fa-share-from-square',
+    },
+    {
+      backgroundImage: 'images/APIDevelopmentandIntegration.jpeg',
+      heading: 'API Development and Integration',
+      description: 'API Development and Integration involve creating interfaces to connect software systems, enabling seamless communication and data exchange across applications.',
+      icon: 'fa-solid fa-share-from-square',
+    },
+    {
+      backgroundImage: 'images/authenticationandAuthorization.jpeg',
+      heading: 'Authentication and Authorization',
+      description: 'Authentication verifies user identity, while authorization determines access rights, ensuring secure and controlled user interactions within systems and applications.',
+      icon: 'fa-solid fa-share-from-square',
+    },
+  ];
 }
